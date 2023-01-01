4e6297b30f339354a0a21dcdb21d6ef97ad2a4dc
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2023-01-01 17:44:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +21,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
@@ -30,3 +30,26 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/:time", (req, res) => {
+  var date = new Date(req.params.time);
+  var date2 = new Date(parseInt(req.params.time));
+  console.log(date)
+  if (date == "Invalid Date" && date2 == "Invalid Date") {
+    res.json({ error: "Invalid Date" })
+    console.log("0")
+  }
+  else if (date == "Invalid Date") {
+    console.log("1")
+    res.json({ unix: parseInt(date2.getTime()), utc: date2.toUTCString() })
+  }
+  else {
+    console.log("2")
+
+    res.json({ unix: parseInt(date.getTime()), utc: date.toUTCString() })
+  }
+})
+app.get("/api", (req, res) => {
+  date = new Date()
+  res.json({ unix: date.getTime(), utc: date.toUTCString() })
+})
